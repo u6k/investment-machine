ee445a015b29f7d88612234cdc3a2325eef63b12
u6k <u6k.apps@gmail.com>
2018-04-05 15:26:18 +0900

    impl download page links.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,42 @@
+require "open-uri"
+require "nokogiri"
+
 class Stock < ApplicationRecord
+
+  def self.download_page_links
+    url = "https://kabuoji3.com/stock/"
+    http_header = {
+      "User-Agent" => "curl/7.54.0",
+      "Accept" => "*/*"
+    }
+
+    charset = nil
+    html = open(url, http_header) do |f|
+      charset = f.charset
+      f.read
+    end
+
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+
+#    stock_table_lines = doc.xpath("//table[@class='stock_table']/tbody/tr/td/a")
+
+#    stocks = []
+#    stock_table_lines.each do |stock_table_line|
+#      stocks << {
+#        "code" => stock_table_line.text[0..3],
+#        "name" => stock_table_line.text[5..-1]
+#      }
+#    end
+
+    pager_lines = doc.xpath("//ul[@class='pager']/li/a")
+
+    page_links = []
+    pager_lines.each do |pager_line|
+      page_links << pager_line["href"]
+    end
+
+    page_links
+
+  end
+
 end
--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -11,4 +11,13 @@ class StockTest < ActiveSupport::TestCase
     assert_equal 1, Stock.all.length
   end
 
+  test "download page links" do
+    page_links = Stock.download_page_links
+
+    assert page_links.length > 0
+    page_links.each do |l|
+      assert l.match(/^\?page=/)
+    end
+  end
+
 end
