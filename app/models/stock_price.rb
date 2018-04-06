1a34df263c6ddc62291d50d953e0b6ba6c864b34
u6k <u6k.apps@gmail.com>
2018-04-06 19:11:24 +0900

    impl stock_price.download_years.

--- app/models/stock_price.rb
+++ app/models/stock_price.rb
@@ -1,3 +1,19 @@
 class StockPrice < ApplicationRecord
   belongs_to :stock
+
+  def self.download_years(ticker_symbol, transaction_id)
+    url = "https://kabuoji3.com/stock/#{ticker_symbol}/"
+    object_key = "#{transaction_id}/stock_detail_#{ticker_symbol}.html"
+    doc = Stock.download_and_parse_page(url, object_key)
+
+    year_nodes = doc.xpath("//ul[@class='stock_yselect mt_10']/li/a")
+
+    years = []
+    year_nodes.each do |year_node|
+      years << year_node.text.to_i if year_node.text.match(/^[0-9]{4}$/)
+    end
+
+    years.sort
+  end
+
 end
