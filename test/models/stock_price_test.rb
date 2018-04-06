cddccbb36f48a551e673a2c06e338f79fba74a17
u6k <u6k.apps@gmail.com>
2018-04-06 19:10:36 +0900

    impl stock_price test.

--- test/models/stock_price_test.rb
+++ test/models/stock_price_test.rb
@@ -2,22 +2,45 @@ require 'test_helper'
 
 class StockPriceTest < ActiveSupport::TestCase
 
-  test "save stock_price" do
-    assert_equal 0, Stock.all.length
-    assert_equal 0, StockPrice.all.length
+  test "download years for stock" do
+    transaction_id = Stock._generate_transaction_id
+    ticker_symbol = "1301"
 
-    stock = Stock.new
+    years = StockPrice.download_years(ticker_symbol, transaction_id)
 
-    stock_price = StockPrice.new
-    stock_price.stock = stock
-    assert stock_price.save
+    assert_equal 36, years.length
+    (1983..2018).each do |year|
+      assert_includes years, year
+    end
 
-    stock_price = StockPrice.new
-    stock_price.stock = stock
-    assert stock_price.save
+    bucket = Stock._get_s3_bucket
+    assert bucket.object("#{transaction_id}/stock_detail_1301.html").exists?
+  end
 
-    assert_equal 1, Stock.all.length
-    assert_equal 2, StockPrice.all.length
+  test "download csv with ticker symbol and year" do
+    transaction_id = Stock._generate_transaction_id
+    ticker_symbol = "1301"
+    year = 2001
+
+    stock_prices_data = StockPrice.download_stock_prices(ticker_symbol, year, transaction_id)
+
+    expected_date_min = Date.new(2001, 1, 1)
+    expected_date_max = Date.new(2002, 1, 1)
+
+    assert stock_prices_data.length > 0
+    stock_prices_data.each do |stock_price|
+      assert stock_price[:date].date?
+      assert stock_price[:date] >= expected_date_min && stock_price[:date] < expected_date_max
+      assert stock_price[:opening_price].integer?
+      assert stock_price[:high_price].integer?
+      assert stock_price[:low_price].integer?
+      assert stock_price[:close_price].integer?
+      assert stock_price[:turnover].integer?
+      assert stock_price[:adjustment_value].integer?
+    end
+
+    bucket = Stock._get_s3_bucket
+    assert bucket.object("#{transaction_id}/stock_price_1301_2001.csv").exists?
   end
 
 end
