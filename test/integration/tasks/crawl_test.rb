c24825bd1215463f1fa7840c895fe4f75acbef1e
u6k <u6k.apps@gmail.com>
2018-04-09 07:57:44 +0900

    impl crawl:stock_prices test. (no impl)

--- test/integration/tasks/crawl_test.rb
+++ test/integration/tasks/crawl_test.rb
@@ -11,4 +11,24 @@ class CrawlTest < ActionDispatch::IntegrationTest
     Rake::Task["crawl:hello"].invoke(ticker_symbol: "1002", year: 2018)
   end
 
+  test "get all stocks" do
+    # TODO
+  end
+
+  test "get stock_prices ticker_symbol=1301, year=2018" do
+    # TODO
+  end
+
+  test "get stock_prices ticker_symbol=1301, year=nil" do
+    # TODO
+  end
+
+  test "get stock_prices ticker_symbol=nil, year=2018" do
+    # TODO
+  end
+
+  test "get stock_prices ticker_symbol=nil, year=nil" do
+    # TODO
+  end
+
 end
