4719a05b9a248ca33d2333126011181e786d977e
u6k <u6k.apps@gmail.com>
2018-04-09 09:02:04 +0900

    impl sample mock.

--- test/integration/tasks/crawl_test.rb
+++ test/integration/tasks/crawl_test.rb
@@ -1,18 +1,27 @@
 require 'test_helper'
+require 'minitest/autorun'
 
 class CrawlTest < ActionDispatch::IntegrationTest
 
-  def setup
-    Myapp::Application.load_tasks
-  end
+  test "hello" do
+    mock = MiniTest::Mock.new
+    mock.expect :call, "foo", ["xxx"]
 
-  test "test" do
-    # TODO
-    Rake::Task["crawl:hello"].invoke(ticker_symbol: "1002", year: 2018)
+    Stock.stub :download_index_page, mock do
+      key = Stock.download_index_page("xxx")
+      assert_equal "foo", key
+    end
   end
 
   test "get all stocks" do
-    # TODO
+    assert_equal 0, Stock.all.length
+    assert_equal 0, StockPrice.all.length
+
+    Myapp::Application.load_tasks
+    Rake::Task["crawl:stocks"].invoke
+
+    assert Stock.all.length > 3000
+    assert_equal 0, StockPrice.all.length
   end
 
   test "get stock_prices ticker_symbol=1301, year=2018" do
