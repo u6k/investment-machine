2ebd7f01a63baeb448cddb403821a4bd98351bcd
u6k <u6k.apps@gmail.com>
2018-04-09 20:30:22 +0900

    impl crawl:stock_prices without test.

--- test/integration/tasks/crawl_test.rb
+++ test/integration/tasks/crawl_test.rb
@@ -3,41 +3,14 @@ require 'minitest/autorun'
 
 class CrawlTest < ActionDispatch::IntegrationTest
 
-  test "hello" do
-    mock = MiniTest::Mock.new
-    mock.expect :call, "foo", ["xxx"]
-
-    Stock.stub :download_index_page, mock do
-      key = Stock.download_index_page("xxx")
-      assert_equal "foo", key
-    end
-  end
-
-  test "get all stocks" do
-    assert_equal 0, Stock.all.length
-    assert_equal 0, StockPrice.all.length
-
-    Myapp::Application.load_tasks
-    Rake::Task["crawl:stocks"].invoke
-
-    assert Stock.all.length > 3000
-    assert_equal 0, StockPrice.all.length
-  end
-
-  test "get stock_prices ticker_symbol=1301, year=2018" do
-    # TODO
-  end
-
-  test "get stock_prices ticker_symbol=1301, year=nil" do
-    # TODO
-  end
-
-  test "get stock_prices ticker_symbol=nil, year=2018" do
-    # TODO
-  end
-
-  test "get stock_prices ticker_symbol=nil, year=nil" do
-    # TODO
-  end
+  # TODO
+  #test "hello" do
+  #  mock = MiniTest::Mock.new
+  #  mock.expect :call, "foo", ["xxx"]
+  #  Stock.stub :download_index_page, mock do
+  #    key = Stock.download_index_page("xxx")
+  #    assert_equal "foo", key
+  #  end
+  #end
 
 end
