0700739239cc3123e8f1c46f2262830e97f3887d
u6k <u6k.apps@gmail.com>
2018-05-09 17:33:45 +0900

    [WIP] impl topix model and test.

--- app/models/topix.rb
+++ app/models/topix.rb
@@ -1,2 +1,15 @@
 class Topix < ApplicationRecord
+
+  def self.download_topix_csv(date_from, date_to)
+    raise "TODO"
+  end
+
+  def self.get_topixes(object_key)
+    raise "TODO"
+  end
+
+  def self.import(topixes)
+    raise "TODO"
+  end
+
 end
