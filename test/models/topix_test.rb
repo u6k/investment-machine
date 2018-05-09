ee2246b3f2f2d28764c67cfe9de7ae11be701ab2
u6k <u6k.apps@gmail.com>
2018-05-09 17:13:51 +0900

    [WIP] impl topix model test.

--- test/models/topix_test.rb
+++ test/models/topix_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class TopixTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "download csv and get topixes" do
+    raise "TODO"
+  end
+
+  test"import topixes and overwrite" do
+    raise "TODO"
+  end
+
 end
