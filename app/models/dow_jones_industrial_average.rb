9f088d2909cf12e5724924028245fc0bce372567
u6k <u6k.apps@gmail.com>
2018-05-10 01:55:05 +0900

    [WIP] impl DJIA model.

--- app/models/dow_jones_industrial_average.rb
+++ app/models/dow_jones_industrial_average.rb
@@ -1,2 +1,15 @@
 class DowJonesIndustrialAverage < ApplicationRecord
+
+  def self.download_djia_csv(date_from, date_to)
+    raise "TODO"
+  end
+
+  def self.get_djias(object_key)
+    raise "TODO"
+  end
+
+  def self.import(djias)
+    raise "TODO"
+  end
+
 end
