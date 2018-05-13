76d6603ab11304a38220fc18536a4a70ab5644c6
u6k <u6k.apps@gmail.com>
2018-05-13 21:44:51 +0900

    [WIP] impl download feed.

--- app/models/wertpapier_report.rb
+++ app/models/wertpapier_report.rb
@@ -1,2 +1,13 @@
 class WertpapierReport < ApplicationRecord
+
+  validates :ticker_symbol, presence: true, format: { with: /\d{4}/i }
+  validates :entry_id, presence: true, format: { with: /.+/i }
+  validates :title, presence: true, format: { with: /.+/i }
+  validates :content_type, presence: true, format: { with: /.+/i }
+  validates :entry_updated, presence: true
+
+  def self.download_feed(ticker_symbol)
+    raise "TODO" # TODO
+  end
+
 end
--- test/models/wertpapier_report_test.rb
+++ test/models/wertpapier_report_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class WertpapierReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    bucket = Stock._get_s3_bucket
+    bucket.objects.batch_delete!
+  end
+
+  test "download feed" do
+    bucket = Stock._get_s3_bucket
+
+    keys = WertpapierReport.download_feed("1301")
+    assert_equal "wertpapier_feed_1301.atom", keys[:original]
+    assert_match /^wertpapier_feed_1301\.atom\.bak_[0-9]{14}/, keys[:backup]
+    assert bucket.object(keys[:original]).exists?
+    assert bucket.object(keys[:backup]).exists?
+  end
+
 end
