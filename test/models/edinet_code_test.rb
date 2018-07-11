210d3b8eb70c9ff91db2dd1e5162cff7cbf3f710
u6k <u6k.apps@gmail.com>
2018-07-11 14:55:51 +0900

    impl download edinet code list

--- test/models/edinet_code_test.rb
+++ test/models/edinet_code_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class EdinetCodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    bucket = Stock._get_s3_bucket
+    bucket.objects.batch_delete!
+  end
+
+  test "download edinet code list zip" do
+    # precondition
+    bucket = Stock._get_s3_bucket
+
+    assert_equal 0, EdinetCode.all.length
+
+    # execute
+    result = EdinetCode.download_edinet_code_list
+
+    # postcondition
+    assert result[:data].length > 0
+
+    assert_equal 0, EdinetCode.all.length
+  end
+
 end
