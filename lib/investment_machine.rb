60b3dd61bc04ee8098585c3c8f644e977a01afe6
u6k <u6k.apps@gmail.com>
2019-03-23 23:25:09 +0900

    Added: logger

--- lib/investment_machine.rb
+++ lib/investment_machine.rb
@@ -1,12 +1,26 @@
 require "thor"
 
 require "investment_machine/version"
+require "parser/stock_list_page_parser"
 
 module InvestmentMachine
   class CLI < Thor
     desc "version", "Display version"
     def version
       puts InvestmentMachine::VERSION
+    end
+  end
+
+  class AppLogger
+    @@logger = nil
+
+    def self.get_logger
+      if @@logger.nil?
+        @@logger = Logger.new(STDOUT)
+        @@logger.level = ENV["INVESTMENT_LOGGER_LEVEL"] if ENV.has_key?("INVESTMENT_LOGGER_LEVEL")
+      end
+
+      @@logger
     end
   end
 end
