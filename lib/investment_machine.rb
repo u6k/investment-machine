653bad92e162b942483baa35b0308d953b8129c9
u6k <u6k.apps@gmail.com>
2019-03-23 11:22:04 +0900

    Added: impl cli

--- lib/investment_machine.rb
+++ lib/investment_machine.rb
@@ -1,6 +1,13 @@
+require "thor"
+
 require "investment_machine/version"
 
 module InvestmentMachine
-  class Error < StandardError; end
-  # Your code goes here...
+  class CLI < Thor
+    desc "version", "Display version"
+    def version
+      puts InvestmentMachine::VERSION
+    end
+  end
 end
+
--- spec/investment_machine_spec.rb
+++ spec/investment_machine_spec.rb
@@ -3,3 +3,12 @@ RSpec.describe InvestmentMachine do
     expect(InvestmentMachine::VERSION).not_to be nil
   end
 end
+
+RSpec.describe InvestmentMachine::CLI do
+  it "is version" do
+    stdout = capture(:stdout) { InvestmentMachine::CLI.new.invoke("version") }
+
+    expect(stdout).to eq "#{InvestmentMachine::VERSION}\n"
+  end
+end
+
