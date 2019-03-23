6dd0e176c0f0c2ea643189707583157761bf9e06
u6k <u6k.apps@gmail.com>
2019-03-23 10:57:03 +0900

    Changed: replace rails to gem

---
+++ lib/investment_machine.rb
@@ -0,0 +1,6 @@
+require "investment_machine/version"
+
+module InvestmentMachine
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/investment_machine_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe InvestmentMachine do
+  it "has a version number" do
+    expect(InvestmentMachine::VERSION).not_to be nil
+  end
+end
