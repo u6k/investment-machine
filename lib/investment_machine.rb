41d408672aa8b10281bc76952704c17448abfc5f
u6k <u6k.apps@gmail.com>
2019-03-24 01:05:25 +0900

    Added: CLI

--- lib/investment_machine.rb
+++ lib/investment_machine.rb
@@ -9,6 +9,29 @@ module InvestmentMachine
     def version
       puts InvestmentMachine::VERSION
     end
+
+    desc "crawl", "Crawl stocks"
+    method_option :s3_access_key
+    method_option :s3_secret_key
+    method_option :s3_region, default: "us-east-1"
+    method_option :s3_bucket
+    method_option :s3_endpoint, default: "https://s3.amazonaws.com"
+    method_option :s3_force_path_style, default: false
+    method_option :interval, default: "1.0"
+    method_option :entrypoint_url, default: "https://kabuoji3.com/stock/"
+    def crawl
+      downloader = Crawline::Downloader.new("investment-machine/#{InvestmentMachine::VERSION} (https://github.com/u6k/investment-machine)")
+
+      repo = Crawline::ResourceRepository.new(options.s3_access_key, options.s3_secret_key, options.s3_region, options.s3_bucket, options.s3_endpoint, options.s3_force_path_style, nil)
+
+      parsers = {
+        /^https:\/\/kabuoji3\.com\/stock\/(\?page=\d{1,2})?$/ => Parser::StockListPageParser
+      }
+
+      engine = Crawline::Engine.new(downloader, repo, parsers, options.interval.to_i)
+
+      engine.crawl(options.entrypoint_url)
+    end
   end
 
   class AppLogger
